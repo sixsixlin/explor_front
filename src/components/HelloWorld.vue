26918b0eabc5a8e2452366d678639d8f1e5dac20
Aurora <487408404@qq.com>
2023-06-05 16:05:11 +0800

    1

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,47 +1,33 @@
 <script setup lang="ts">
-import {onMounted, reactive, ref} from 'vue'
-import {getUserInfo} from "../services/user.ts";
-import {findVarietyDict} from "../services/article.ts";
-
-
+import { onMounted, reactive, ref } from 'vue';
+import { findArticle } from "../services/article.ts";
+const params=reactive({
+  articleLableID:'',
+  articleTitle:'农业'
+})
 defineProps<{ msg: string }>()
 
 const count = ref(0)
 
 onMounted(async ()=>{
-  try {
-    const data = await findVarietyDict()
-    console.log(data)
-  }catch (err){
-    console.log(err)
-  }
+  await getUser()
 })
+async function getUser() {
+  try {
+    const tableData =await findArticle(params)
+    console.log(tableData);
+    
+  }catch (err){
+    // console.log(err)
+  }
+}
 
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+ 
+  \
 </template>
 
 <style scoped>
--- src/services/article.ts
+++ src/services/article.ts
@@ -1,5 +1,9 @@
 import axios from '../utils/axios.ts'
-
-export function findVarietyDict(){
-    return axios.post('/article/findVarietyDict')
+/**
+ * 根据文章标签或者 文章主题查找文章
+ * @param data 
+ * @returns 
+ */
+export function findArticle(data){
+    return axios.post('/article/findArticle', data)
 }
